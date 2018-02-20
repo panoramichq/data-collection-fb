266c8270cecd2f9c942e6b6cd76a29315d15d1ea
Daniel Dotsenko <daniel.dotsenko@operam.com>
2018-02-20 14:57:51 -0800

    massage Entities-by-AA Celery task definitions

--- oozer/entities/facebook_entity_collector_tasks.py
+++ oozer/entities/facebook_entity_collector_tasks.py
@@ -1,9 +1,7 @@
 from common.celeryapp import get_celery_app
+from common.enums.entity import Entity
 
-from .facebook_entity_collector import (
-    collect_entities_for_adaccount,
-    ENTITY_CAMPAIGN
-)
+from .facebook_entity_collector import collect_entities_for_adaccount
 
 
 app = get_celery_app()
@@ -21,5 +19,35 @@ def fb_entities_adaccount_campaigns(job_scope, context):
     # TODO: possibly rehydrate JobScope for easier manipulation
 
     collect_entities_for_adaccount(
-        ENTITY_CAMPAIGN, job_scope, context
+        Entity.Campaign, job_scope, context
+    )
+
+
+@app.task
+def fb_entities_adaccount_adsets(job_scope, context):
+    """
+    Collect all campaign entities for a given adaccount
+
+    :param dict job_scope: The dict representation of JobScope
+    :param dict context:
+    """
+    # TODO: possibly rehydrate JobScope for easier manipulation
+
+    collect_entities_for_adaccount(
+        Entity.AdSet, job_scope, context
+    )
+
+
+@app.task
+def fb_entities_adaccount_ads(job_scope, context):
+    """
+    Collect all campaign entities for a given adaccount
+
+    :param dict job_scope: The dict representation of JobScope
+    :param dict context:
+    """
+    # TODO: possibly rehydrate JobScope for easier manipulation
+
+    collect_entities_for_adaccount(
+        Entity.Ad, job_scope, context
     )
