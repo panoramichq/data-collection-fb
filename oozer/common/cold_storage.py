e3c8bff18c3e2506135d91ce0fb8982536f001de
Daniel Dotsenko <daniel.dotsenko@operam.com>
2018-02-25 21:20:27 -0800

    teach cold_storage.store to pad keys with chunk number

--- oozer/common/cold_storage.py
+++ oozer/common/cold_storage.py
@@ -17,11 +17,22 @@ s3://operam-reports/facebook/2d700d-1629501014003404/fb_insights_campaign_daily/
 """
 import boto3
 import hashlib
-import json
+# ujson is faster for massive amounts of small data units
+# which is actually the pattern we have - yielding small datum per normative
+# task or small batches of small datums.
+# Biggest problem (for us) with ujson is its handling of very very large numbers
+# However, since this code base is dealing with facebook and they committed to
+# sending very large numbers as strings, as long as we do NOT convert
+# super large (massive spend, or Entity IDs) from stringified form to longs
+# we should be fine (and substantially faster on shoving data out us).
+# http://artem.krylysov.com/blog/2015/09/29/benchmark-python-json-libraries/
+import ujson as json
+
 from datetime import datetime
 
 import config.aws
 import config.build
+
 from oozer.common.job_scope import JobScope
 
 
@@ -31,7 +42,7 @@ _s3 = boto3.resource('s3', endpoint_url=config.aws.S3_ENDPOINT)
 _bucket = _s3.Bucket(config.aws.S3_BUCKET_NAME)
 
 
-def _job_scope_to_storage_key(job_scope):
+def _job_scope_to_storage_key(job_scope, chunk_marker=0):
     """
     Puts together the S3 object key we need for given report data. This is
     just a helper function
@@ -45,20 +56,22 @@ def _job_scope_to_storage_key(job_scope):
     report_run_time = datetime.utcnow()
     zulu_time = report_run_time.strftime('%Y-%m-%dT%H:%M:%SZ')
 
+    job_id = job_scope.job_id + ('-'+str(chunk_marker) if chunk_marker else '')
+
     key = f'{job_scope.platform}/' \
           f'{prefix}-{job_scope.ad_account_id}/' \
           f'{job_scope.report_type}/' \
           f'{report_run_time.strftime("%Y")}/' \
           f'{report_run_time.strftime("%m")}/' \
           f'{report_run_time.strftime("%d")}/' \
-          f'{zulu_time}-{job_scope.job_id}.json'
+          f'{zulu_time}-{job_id}.json'
 
     # need this to make it work in local fake S3
     # return key.replace(':', '_')
 
     return key
 
-def store(data, job_scope):
+def store(data, job_scope, chunk_marker=0):
     """
     Adds the item to the current buffer (by JSON dumping it) and Uploads the
     buffer to S3 under a constructed key
@@ -67,11 +80,15 @@ def store(data, job_scope):
     reason behind that is to be more or less oblivious to what it is we are
     actually storing and not have internal knowledge about the data types.
 
-    :param dict data: The raw data to store
+    :param data: The raw data to store
+    :type data: dict or list
     :param JobScope job_scope: The job scope information
+    :param chunk_marker: indicator / label hinting that this payload is
+        just some portion of this job's total returned data and that this
+        particular chunk must be saved under an ID that includes the chunk_marker.
     :return string: The key used to store the data
     """
-    key = _job_scope_to_storage_key(job_scope)
+    key = _job_scope_to_storage_key(job_scope, chunk_marker)
 
     _bucket.put_object(
         Key=key,
