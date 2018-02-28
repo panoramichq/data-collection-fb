092439670a65a5ceecc03f57caa3d12da98284d0
Daniel Dotsenko <daniel.dotsenko@operam.com>
2018-02-27 18:55:43 -0800

    Logging - adding module, line info to log output
    muting anything that is lower than Warning on `botocore` and `pynamodb`

--- common/configure_logging.py
+++ common/configure_logging.py
@@ -7,4 +7,19 @@ def configure_logging(level=LEVEL):
     logger.setLevel(level)
     handler = logging.StreamHandler()
     handler.setLevel(level)
+    handler.setFormatter(
+        logging.Formatter(
+            # %(pathname)s
+            '%(relativeCreated)d  [%(name)s.%(funcName)s:%(lineno)d] %(message)s'
+        )
+    )
     logger.addHandler(handler)
+
+    to_mute = [
+        'botocore',
+        'celery.app.trace',
+        'celery.worker.strategy',
+        'pynamodb',
+    ]
+    for module in to_mute:
+        logging.getLogger(module).setLevel(logging.WARNING)
