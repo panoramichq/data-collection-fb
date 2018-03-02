71d666534d825cd899f79121e32c82338a07fffc
Tomas Plesek <tomas.plesek@operam.com>
2018-03-02 15:25:23 +0100

    Redis cluster option

--- common/connect/redis.py
+++ common/connect/redis.py
@@ -1,7 +1,17 @@
-import redis
-
 from config import redis as redis_config
 
 
 def get_redis():
-    return redis.Redis.from_url(redis_config.URL)
+    """
+    Obtains either standard redis connection or cluster aware connection. These
+    are mutually exclusive. Using a simple flag, we can potentially add some
+    auto-sniff-cluster functionality if we want to, won't be that hard
+
+    :return ConnectionPool | ClusterConnectionPool: The redis connection pool
+    """
+    if redis_config.CLUSTER:
+        import rediscluster
+        return rediscluster.RedisCluster.from_url(redis_config.URL)
+    else:
+        import redis
+        return redis.Redis.from_url(redis_config.URL)
--- config/redis.py
+++ config/redis.py
@@ -1,4 +1,14 @@
 URL = 'redis://redis:6379'
 
+# If you use this setting, the non-Celery redis will connect to a redis cluster
+# as opposed to a normal Redis.
+# As these drivers are not compatible with each other, use against an actual
+# cluster only.
+#
+# When using AWS ElastiCache connect only to the "Configuration Endpoint" as
+# visible in the AWS console (hence we don't need multiple host addresses)
+CLUSTER = False
+
+
 from common.updatefromenv import update_from_env
 update_from_env(__name__)
