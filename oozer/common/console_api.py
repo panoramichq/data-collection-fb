657fc77514c2081b99140d556e26cd5f0f0395be
Roman Roštár <roman.rostar@gmail.com>
2018-03-06 13:17:46 +0100

    FIx pep compliance

--- oozer/common/console_api.py
+++ oozer/common/console_api.py
@@ -11,8 +11,8 @@ class ConsoleApi:
     def get_active_accounts(self):
         response = requests.get(
             f'{URL}/api/projects/platform-accounts',
-            headers={'x-auth-token': TOKEN },
-            params= { 'platform': 'facebook' }
+            headers={'x-auth-token': TOKEN},
+            params= {'platform': 'facebook'}
         )
 
         return response.json()
