91f31d8e0441b6c05b00ffc962a3d1456c50bfce
Roman Roštár <roman.rostar@gmail.com>
2018-03-08 15:57:54 +0100

    Surround ad account extracion job in try catch

--- oozer/entities/extract_adaccounts_task.py
+++ oozer/entities/extract_adaccounts_task.py
@@ -1,11 +1,13 @@
 import logging
 
 from common.celeryapp import get_celery_app
+from oozer.common.enum import JobStatus
 from oozer.common.job_context import JobContext
 from oozer.common.job_scope import JobScope
 from oozer.common.console_api import ConsoleApi
 from common.store.entities import FacebookAdAccountEntity
 
+from oozer.common.report_job_status_task import report_job_status_task
 from common.store.scope import FacebookAdAccountScope, FacebookToken, DEFAULT_SCOPE
 
 from config.operam_console_api import TOKEN as CONSOLE_API_TOKEN
@@ -22,22 +24,39 @@ def extract_adaccounts_task(job_scope, job_context):
     :param JobContext job_context:
     """
 
+    from pynamodb.exceptions import PutError
+
+
     logger.info(
         f'{job_scope} started'
     )
+    report_job_status_task.delay(JobStatus.Start, job_scope)
 
-    console_client = ConsoleApi(CONSOLE_API_TOKEN)
-    accounts = console_client.get_active_accounts()
+    try:
+        console_client = ConsoleApi(CONSOLE_API_TOKEN)
+        accounts = console_client.get_active_accounts()
 
-    from pprint import pprint
-    pprint(accounts)
+        for ad_account in accounts:
+            # TODO: maybe create a normative job scope that says ("extracting ad account")
+            try:
+                # TODO: maybe rather get the entity first and update insert accordingly / if it has change
+                record = FacebookAdAccountEntity.upsert(
+                    DEFAULT_SCOPE,
+                    ad_account['ad_account_id'],
+                    is_active=True,
+                    timezone=ad_account['timezone'],
+                    updated_by_sweep_id=job_scope.sweep_id
+                )
+            except PutError as ex:
+                # TODO: report_job_status_task.delay(ConsoleExtractionJobStatus.UpsertError, normative_job_scope)
+                ex_str = str(ex)
+                if 'ProvisionedThroughputExceededException' in ex_str:
+                    logger.info(ex_str)
+                else:
+                    raise
 
-    for ad_account in accounts:
-        record = FacebookAdAccountEntity.upsert(
-            DEFAULT_SCOPE,
-            ad_account['ad_account_id'],
-            is_active=True,
-            timezone=ad_account['timezone'],
-            updated_by_sweep_id=job_scope.sweep_id
-        )
-        print(record)
+
+        report_job_status_task.delay(JobStatus.Done, job_scope)
+    except Exception as ex:
+        logger.error(str(ex))
+        report_job_status_task.delay(JobStatus.GenericError, job_scope)
