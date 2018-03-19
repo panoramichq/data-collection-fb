1888f46cc76f9ae7199ab312c627e2484e18b5ff
Roman Roštár <roman.rostar@gmail.com>
2018-03-19 15:42:46 +0100

    Align Twitter util patch with how we do it for fb

--- common/twitter/patch.py
+++ common/twitter/patch.py
@@ -2,15 +2,17 @@
 This module is here to runtime-patch problems with Twitter SDK or API
 """
 
+def patch_twitter_sdk():
+    from twitter_ads.account import Account
+    from twitter_ads.creative import PromotedTweet
 
-from twitter_ads.account import Account
-from twitter_ads.creative import PromotedTweet
+    def account_promoted_tweets(self, id=None, **kwargs):
+        """
+        Returns a collection of promotable users available to the
+        current account.
+        """
+        return self._load_resource(PromotedTweet, id, **kwargs)
 
-def account_promoted_tweets(self, id=None, **kwargs):
-    """
-    Returns a collection of promotable users available to the
-    current account.
-    """
-    return self._load_resource(PromotedTweet, id, **kwargs)
+    Account.promoted_tweets = account_promoted_tweets
 
-Account.promoted_tweets = account_promoted_tweets
+
