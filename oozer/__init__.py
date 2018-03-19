84ba627648115040655ca1516129735ae5f7e693
Roman Roštár <roman.rostar@gmail.com>
2018-03-19 17:05:37 +0100

    Patch twitter

--- oozer/__init__.py
+++ oozer/__init__.py
@@ -1,3 +1,6 @@
 # Ensure FB will get patched correctly
 from common.facebook.patch import patch_facebook_sdk
+from common.twitter.patch import patch_twitter_sdk
+
 patch_facebook_sdk()
+patch_twitter_sdk()
