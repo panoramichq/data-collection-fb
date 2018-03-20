2c1bc59a90d7735d1c42c5b12d3e2160e14b9308
Daniel Dotsenko <daniel.dotsenko@operam.com>
2018-03-20 01:49:19 -0700

    change report_job_status task to use JobReport table + sync_status out

--- oozer/common/report_job_status.py
+++ oozer/common/report_job_status.py
@@ -1,8 +1,15 @@
 import logging
 
+from datetime import datetime
+
+import config.application
+
 from common.enums.failure_bucket import FailureBucket
-from oozer.common.job_scope import JobScope
+from common.enums.reporttype import ReportType
+from common.store.jobreport import JobReport
+from oozer.common import cold_storage
 from oozer.common.enum import FacebookJobStatus
+from oozer.common.job_scope import JobScope
 
 
 logger = logging.getLogger(__name__)
@@ -18,7 +25,7 @@ def report_job_status(stage_id, job_scope):
     :param JobScope job_scope: The job scope that is attached to this particular
         report
     """
-    # This will be refactored at the source
+
     # TODO: move this import to top of file then
     from oozer.looper import SweepStatusTracker
 
@@ -51,15 +58,68 @@ def report_job_status(stage_id, job_scope):
                     f'#{job_scope.sweep_id} Done status report "{job_scope.job_id}": "{status_bucket}"'
                 )
 
-    # FacebookSweepEntityReport(
-    #     job_scope.sweep_id,
-    #     job_scope.job_id,
-    #     report_type=job_scope.report_type,
-    #     ad_account_id=job_scope.ad_account_id,
-    #     entity_id=job_scope.entity_id,
-    #     entity_type=job_scope.entity_type,
-    #     stage_id=stage_id,
-    #     failure_bucket=status_bucket
-    # ).save()
+    if isinstance(stage_id, int):
 
-    logger.debug(f"#:job {job_scope.job_id} reported completion stage {stage_id} (bucketed as {status_bucket})")
+        is_done = False
+        data = None
+
+        if stage_id == FacebookJobStatus.Done:
+            data = dict(
+                last_success_dt=datetime.utcnow(),
+                last_success_sweep_id=job_scope.sweep_id,
+            )
+            is_done = True
+
+        elif stage_id > 0:
+            data = dict(
+                last_progress_dt=datetime.utcnow(),
+                last_progress_stage_id=stage_id,
+                last_progress_sweep_id=job_scope.sweep_id
+            )
+
+        elif stage_id < 0:
+            data = dict(
+                last_failure_dt=datetime.utcnow(),
+                last_failure_stage_id=stage_id,
+                last_failure_sweep_id=job_scope.sweep_id,
+                # last_failure_error=?
+                last_failure_bucket=status_bucket
+            )
+
+        # important to use upsert, not .save()
+        # .save() saves entire model, including zeroing out the optional fields
+        # not set here. Must use blind update that updates only the fields we specify.
+        if data:
+
+            JobReport.upsert(
+                job_scope.job_id,
+                **data
+            )
+
+        # job_scope.namespace == JobScope.namespace means equal to default value
+        # which is the value for external platform namespace
+        # We don't want to report jobs for other namespaces (yet)
+        if is_done and job_scope.namespace == JobScope.namespace:
+
+            reporting_job_scope = JobScope(
+                sweep_id=job_scope.sweep_id,
+                namespace=config.application.UNIVERSAL_ID_SYSTEM_NAMESPACE,
+                ad_account_id=job_scope.ad_account_id,
+                report_type=ReportType.sync_status
+            )
+
+            cold_storage.store(
+                dict(
+                    job_id=job_scope.job_id,
+                    # status='done',  # discuss value and attr name with Mike
+                    ad_account_id=job_scope.ad_account_id,
+                    entity_type=job_scope.entity_type,
+                    entity_id=job_scope.entity_id,
+                    report_type=job_scope.report_type,
+                    report_variant=job_scope.report_variant,
+                    range_start=job_scope.range_start,
+                    range_end=job_scope.range_end,
+                    platform_namespace=job_scope.namespace,
+                ),
+                reporting_job_scope
+            )
