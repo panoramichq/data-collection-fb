2bc75dbc27a99a3f1bfe9e5218a9ffd95ea304a1
Daniel Dotsenko <daniel.dotsenko@operam.com>
2018-03-25 21:24:23 -0700

    prune config.dynamodb - remove unused table names

--- config/dynamodb.py
+++ config/dynamodb.py
@@ -9,13 +9,8 @@ FB_AD_ACCOUNT_SCOPE_TABLE = 'FacebookAdAccountScope'
 FB_AD_ENTITY_TABLE = 'FacebookAdEntity'
 FB_ADSET_ENTITY_TABLE = 'FacebookAdsetEntity'
 FB_CAMPAIGN_ENTITY_TABLE = 'FacebookCampaignEntity'
-FB_SWEEP_ENTITY_REPORT_TYPE_TABLE = 'SweepEntityReport'
 FB_TOKEN_TABLE = 'FacebookPlatformToken'
-JOB_REPORT_ENTITY_EXPECTATION_AD_ACCOUNT_INDEX_TABLE = 'JobReportEntityExpectationAdAccountIndex'
-JOB_REPORT_ENTITY_EXPECTATION_TABLE = 'JobReportEntityExpectation'
 JOB_REPORT_TABLE = 'JobReport'
-JOB_REPORT_FAILURE_TABLE = 'JobReportFailure'
-
 
 from common.updatefromenv import update_from_env
 update_from_env(__name__)
