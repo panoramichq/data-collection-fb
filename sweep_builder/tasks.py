7ea5d47fad0af218d9397caa780fc3cd2df5992f
Daniel Dotsenko <daniel.dotsenko@operam.com>
2018-08-24 10:12:44 -0700

    add time measurement around each prioritized

--- sweep_builder/tasks.py
+++ sweep_builder/tasks.py
@@ -1,5 +1,6 @@
 import logging
 import itertools
+import time
 
 from celery import chord, group
 
@@ -35,6 +36,7 @@ def build_sweep_slice_per_ad_account_task(sweep_id, ad_account_reality_claim, ta
     """
     from .pipeline import iter_pipeline
     from .reality_inferrer.reality import iter_reality_per_ad_account_claim
+    from .data_containers.prioritization_claim import PrioritizationClaim
 
     with TaskGroup.task_context(task_id) as task_context:
 
@@ -43,7 +45,6 @@ def build_sweep_slice_per_ad_account_task(sweep_id, ad_account_reality_claim, ta
             sweep_id=sweep_id,
             ad_account_id=ad_account_reality_claim.ad_account_id
         )
-
 
         reality_claims_iter = itertools.chain(
             [ad_account_reality_claim],
@@ -54,13 +55,23 @@ def build_sweep_slice_per_ad_account_task(sweep_id, ad_account_reality_claim, ta
         with Measure.counter(_measurement_name_base + 'ad_account_loop', tags=_measurement_tags) as cntr:
 
             _step = 1000
+            _before_fetch = time.time()
             for claim in iter_pipeline(sweep_id, reality_claims_iter):
+                Measure.timing(
+                    _measurement_name_base + 'fetch_next',
+                    tags=dict(
+                        entity_type=claim.entity_type,
+                        **_measurement_tags
+                    ),
+                    # sample_rate=0.01
+                )((time.time() - _before_fetch)*1000)
                 cnt += 1
 
                 if cnt % _step == 0:
                     cntr += _step
                     logger.info(f'#{sweep_id}-#{ad_account_reality_claim.ad_account_id}: Queueing up #{cnt}')
 
+                _before_fetch = time.time()
             # because above counter communicates only increments of _step,
             # we need to report remainder --- amount under _step
             cntr += cnt % _step
