7d86620917a5240d24ae3266c269c206d01cb8ed
Daniel Dotsenko <daniel.dotsenko@operam.com>
2018-08-23 17:15:12 -0700

    add expectations per entity type counter

--- sweep_builder/expectation_builder/expectations.py
+++ sweep_builder/expectation_builder/expectations.py
@@ -1,5 +1,6 @@
 from typing import Generator, Union, List
 
+from common.measurement import Measure
 from sweep_builder.data_containers.expectation_claim import ExpectationClaim
 from sweep_builder.data_containers.reality_claim import RealityClaim
 
@@ -19,7 +20,23 @@ def iter_expectations(reality_claims_iter) :
     :return: Generator yielding ExpectationClaim objects
     :rtype: Generator[ExpectationClaim]
     """
+    _measurement_name_base = __name__ + '.iter_expectations.'  # <- function name. adjust if changed
+
     for reality_claim in reality_claims_iter:
         jobs_generators = entity_expectation_generator_map.get(reality_claim.entity_type, [])
-        for jobs_generator in jobs_generators:
-            yield from jobs_generator(reality_claim)
+
+        _measurement_tags = dict(
+            ad_account_id=reality_claim.ad_account_id,
+            entity_type=reality_claim.entity_type
+        )
+
+        # histogram measures min/max/ave per thing.
+        # Here we are trying to measure how given entity type (per ad account)
+        # fans out into expectations.
+        with Measure.histogram(_measurement_name_base + 'expectations_per_reality_claim', tags=_measurement_tags) as cntr:
+            cnt = 0
+            for jobs_generator in jobs_generators:
+                yield from jobs_generator(reality_claim)
+                cnt += 1
+
+            cntr += cnt
