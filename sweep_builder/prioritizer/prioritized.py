89ec03d8ed257f50ab826a3f70dfc1075088119f
Daniel Dotsenko <daniel.dotsenko@operam.com>
2018-08-24 03:35:38 -0700

    add timers around task scoring

--- sweep_builder/prioritizer/prioritized.py
+++ sweep_builder/prioritizer/prioritized.py
@@ -1,5 +1,8 @@
+import time
+
 from typing import Generator
 
+from common.measurement import Measure
 from sweep_builder.data_containers.expectation_claim import ExpectationClaim
 from sweep_builder.data_containers.prioritization_claim import PrioritizationClaim
 
@@ -21,7 +24,19 @@ def iter_prioritized(expectations_iter):
 
     score_calculator = ScoreCalculator()
 
+    _measurement_name_base = __name__ + '.iter_prioritized.'  # <- function name. adjust if changed
+    next_expectation_timer = Measure.timing(
+        _measurement_name_base + 'fetch_expectation',
+        sample_rate=0.1
+    )
+    assign_score_timer = Measure.timing(
+        _measurement_name_base + 'assign_score',
+        sample_rate=0.1
+    )
+
+    _before_next_expectation = time.time()
     for expectation_claim in expectations_iter:
+        next_expectation_timer(time.time() - _before_next_expectation)
 
         # Original logic
         # Temporarily ignored because of the focus in Persister component on last job in the list only
@@ -36,15 +51,21 @@ def iter_prioritized(expectations_iter):
         # effective job that satisfies the normative objective.
         # It's usually the last one in the list. If there is *only* a normative
         # job in the list, it's also the last one in the list.
+        _before_score = time.time()
         last_task_score = score_calculator.assign_score(
             expectation_claim.job_signatures[LAST],
             expectation_claim.timezone
         )
+        assign_score_timer(time.time() - _before_score)
+
         # score of zero is returned for all jobs in the beginning of the expectation_claim.job_signatures
         # list, and only the last job in the list gets an actual score
         job_scores = [0 for i in range(0, len(expectation_claim.job_signatures)-1)] + [last_task_score]
 
-        yield PrioritizationClaim(
-            expectation_claim.to_dict(),
-            job_scores=job_scores
-        )
+        with Measure.timer(_measurement_name_base + 'yield_result', sample_rate=0.1):
+            yield PrioritizationClaim(
+                expectation_claim.to_dict(),
+                job_scores=job_scores
+            )
+
+        _before_next_expectation = time.time()
