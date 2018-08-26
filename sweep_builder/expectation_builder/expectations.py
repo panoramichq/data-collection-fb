51eed28983ee76fc66c225826f6ea3699454fe4f
Daniel Dotsenko <daniel.dotsenko@operam.com>
2018-08-26 11:57:48 -0700

    add measurement fidelity to RealityClaim-to-Expectations bloom, now AdAccount, EntityType + ReportType

--- sweep_builder/expectation_builder/expectations.py
+++ sweep_builder/expectation_builder/expectations.py
@@ -1,5 +1,7 @@
 from typing import Generator, Union, List
+from collections import defaultdict
 
+from common.id_tools import parse_id_parts
 from common.measurement import Measure
 from sweep_builder.data_containers.expectation_claim import ExpectationClaim
 from sweep_builder.data_containers.reality_claim import RealityClaim
@@ -29,17 +31,27 @@ def iter_expectations(reality_claims_iter) :
         # histogram measures min/max/ave per thing.
         # Here we are trying to measure how given entity type (per ad account)
         # fans out into expectations.
-        cnt = 0
-        for jobs_generator in jobs_generators:
-            for expectation_claim in jobs_generator(reality_claim):
-                yield expectation_claim
-                cnt += 1
+        counts = defaultdict(int)
 
-        Measure.histogram(
-            _measurement_name_base + 'expectations_per_reality_claim',
-            tags=dict(
-                ad_account_id=reality_claim.ad_account_id,
-                entity_type=reality_claim.entity_type
-            ),
-            sample_rate=_measurement_sample_rate
-        )(cnt)
+        for jobs_generator in jobs_generators:
+            for expectation_claim in jobs_generator(reality_claim):  # type: ExpectationClaim
+                yield expectation_claim
+
+                if len(expectation_claim.job_signatures):
+                    job_signature = expectation_claim.job_signatures[-1]
+                    report_type = parse_id_parts(job_signature.job_id).report_type
+                else:
+                    report_type = 'unknown'
+
+                counts[report_type] += 1
+
+        for report_type, cnt in counts.items():
+            Measure.histogram(
+                _measurement_name_base + 'expectations_per_reality_claim',
+                tags=dict(
+                    ad_account_id=reality_claim.ad_account_id,
+                    entity_type=reality_claim.entity_type,
+                    report_type=report_type
+                ),
+                sample_rate=_measurement_sample_rate
+            )(cnt)
