95822849e053dd4c77856d9eb4bb3a33da73ae7b
Roman Roštár <roman.rostar@gmail.com>
2018-08-27 15:51:02 +0200

    Add empty token test for collect_account task

--- tests/oozer/entities/test_collect_ad_account.py
+++ tests/oozer/entities/test_collect_ad_account.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from oozer.common.enum import JobStatus
 from oozer.common.job_scope import JobScope
 from oozer.common.report_job_status_task import report_job_status_task
+from common.enums.entity import Entity
 from oozer.entities.collect_adaccount import collect_adaccount
 
 
@@ -28,9 +29,29 @@ class TestCollectAdAccount(TestCase):
         )
 
         with mock.patch.object(report_job_status_task, 'delay') as status_task, \
-            self.assertRaises(ValueError):
+            self.assertRaises(ValueError) as ex_trap:
             collect_adaccount(job_scope, None)
 
+        assert 'Report level' in str(ex_trap.exception)
         assert status_task.called
         parameters, _ = status_task.call_args
-        assert (JobStatus.GenericError, job_scope) == parameters
+        assert (JobStatus.GenericError, job_scope) == parameters, 'Must report status correctly on failure'
+
+    def test_fails_without_a_token(self):
+        job_scope = JobScope(
+            ad_account_id=self.ad_account_id,
+            tokens=[None],
+            report_time=datetime.utcnow(),
+            report_type='entity',
+            report_variant=Entity.AdAccount, # This actually should be set to AdAccount
+            sweep_id='1'
+        )
+
+        with mock.patch.object(report_job_status_task, 'delay') as status_task, \
+            self.assertRaises(ValueError) as ex_trap:
+            collect_adaccount(job_scope, None)
+
+        assert 'token' in str(ex_trap.exception)
+        assert status_task.called
+        parameters, _ = status_task.call_args
+        assert (JobStatus.GenericError, job_scope) == parameters, 'Must report status correctly on failure'
