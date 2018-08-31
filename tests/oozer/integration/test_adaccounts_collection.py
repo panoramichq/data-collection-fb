4ee5175c839b34be3b9a1a7c87fb931d8c3f4ec4
Roman Roštár <roman.rostar@gmail.com>
2018-08-31 11:35:54 +0200

    Fix imports

--- tests/oozer/integration/test_adaccounts_collection.py
+++ tests/oozer/integration/test_adaccounts_collection.py
@@ -3,18 +3,13 @@ from facebook_business.adobjects.adaccount import AdAccount
 from tests.base.testcase import TestCase, integration
 
 from datetime import datetime
-from unittest import skip, mock
 
 from oozer.common.job_scope import JobScope
 from oozer.entities.collect_adaccount import collect_adaccount
 
 from common.enums.entity import Entity
 from config.facebook import TOKEN, AD_ACCOUNT
-from oozer.common.facebook_api import PlatformApiContext, get_default_fields
-
-
-# from oozer.entities.collect_entities_per_adaccount import \
-#     , iter_native_entities_per_adaccount
+from oozer.common.facebook_api import get_default_fields
 
 
 @integration('facebook')
