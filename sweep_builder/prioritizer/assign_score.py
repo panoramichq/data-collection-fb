f86700352b24ab5aa61790df36228a3537ad1129
Lukas Polak <polak.lukas90@gmail.com>
2019-03-01 13:05:42 +0100

    report skipped jobs only once; do not store them in redis

--- sweep_builder/prioritizer/assign_score.py
+++ sweep_builder/prioritizer/assign_score.py
@@ -9,7 +9,6 @@ from common.enums.failure_bucket import FailureBucket
 from common.enums.reporttype import ReportType
 from common.id_tools import parse_id_parts
 from common.job_signature import JobSignature
-from common.measurement import Measure
 from common.store.jobreport import JobReport
 from common.tztools import now_in_tz, now
 from common.math import (
@@ -116,14 +115,6 @@ class ScoreCalculator:
 
         last_success_dt = None if collection_record is None else collection_record.last_success_dt
         if not JobGateKeeper.shall_pass(job_id_parts, last_success_dt=last_success_dt):
-            _measurement_name_base = __name__ + ScoreCalculator.assign_score.__name__
-            measurement_tags = {
-                'ad_account_id': ad_account_id,
-                'entity_type': entity_type,
-                'report_type': report_type,
-                'report_variant': report_variant
-            }
-            Measure.counter(_measurement_name_base + '.gatekeeper_stop_jobs', tags=measurement_tags).increment()
             return JobGateKeeper.JOB_NOT_PASSED_SCORE
 
         score = 0
