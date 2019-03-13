ac0d022398d76e90032eb2b361f03369f7915f3f
Mark Nemec <mrknmc@me.com>
2019-03-13 14:58:14 +0100

    Move is_subject_to_expectation_publication to PrioritizationClaim. Add score field

--- sweep_builder/data_containers/prioritization_claim.py
+++ sweep_builder/data_containers/prioritization_claim.py
@@ -2,10 +2,13 @@ from typing import List, Iterable, Tuple
 from itertools import zip_longest
 
 from common.job_signature import JobSignature
-from sweep_builder.data_containers.expectation_claim import ExpectationClaim
+from common.enums.entity import Entity
+from sweep_builder.data_containers.scorable_claim import ScorableClaim
+
+SUBJECT_TO_EXPECTATION_PUBLICATION = {Entity.Campaign, Entity.AdSet, Entity.Ad}
 
 
-class PrioritizationClaim(ExpectationClaim):
+class PrioritizationClaim(ScorableClaim):
     """
     Used to express a bundle of data representing scored realization of
     need to have certain data point filled.
@@ -15,12 +18,16 @@ class PrioritizationClaim(ExpectationClaim):
     (Used to avoid the need to change all functions in the stack if you need
     to add more data to context from the very bottom of the stack. Just extend this object.)
     """
-
-    # structure matching job_signatures on underlying class
-    # here score for each element in original list is matched in
-    # position in job_scores list.
     job_scores: List[int] = []
+    score: int = None
 
     @property
     def score_job_pairs(self) -> Iterable[Tuple[int, JobSignature]]:
         return zip_longest(self.job_scores, self.job_signatures)
+
+    @property
+    def is_subject_to_expectation_publication(self):
+        return (
+            self.ad_account_id is not None and self.entity_id is not None and
+            self.entity_type in SUBJECT_TO_EXPECTATION_PUBLICATION
+        )
