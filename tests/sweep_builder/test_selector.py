87cfe1b48c624230b0026e37bbe457860e84a4f4
Mark Nemec <mrknmc@me.com>
2019-03-13 14:45:40 +0100

    Add job signature selector

---
+++ tests/sweep_builder/test_selector.py
@@ -0,0 +1,46 @@
+from unittest.mock import patch
+
+from common.enums.entity import Entity
+from common.job_signature import JobSignature
+from sweep_builder.data_containers.expectation_claim import ExpectationClaim
+from sweep_builder.selector import select_signature
+
+
+@patch('sweep_builder.selector._fetch_job_report')
+@patch('sweep_builder.selector.should_select')
+def test_select_signature_default_to_last(mock_should_select, _):
+    expected = JobSignature(job_id='cccc')
+    claim = ExpectationClaim(
+        entity_type=Entity.Ad,
+        entity_id='1111',
+        job_signatures=[
+            JobSignature(job_id='aaaa'),
+            JobSignature(job_id='bbbb'),
+            expected,
+        ]
+    )
+    mock_should_select.side_effect = [False, False, False]
+
+    signature, report = select_signature(claim)
+
+    assert signature == expected
+
+
+@patch('sweep_builder.selector._fetch_job_report')
+@patch('sweep_builder.selector.should_select')
+def test_select_signature_select_first_true(mock_should_select, _):
+    expected = JobSignature(job_id='cccc')
+    claim = ExpectationClaim(
+        entity_type=Entity.Ad,
+        entity_id='1111',
+        job_signatures=[
+            JobSignature(job_id='aaaa'),
+            expected,
+            JobSignature(job_id='bbbb'),
+        ]
+    )
+    mock_should_select.side_effect = [False, True]
+
+    signature, report = select_signature(claim)
+
+    assert signature == expected
