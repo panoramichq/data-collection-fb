cb511250b15dd0b272317e1fcbf5250dbc0e0063
Mark Nemec <mrknmc@me.com>
2019-03-27 13:29:10 +0100

    Add tests for entity node

---
+++ tests/sweep_builder/data_containers/test_entity_node.py
@@ -0,0 +1,24 @@
+from common.enums.entity import Entity
+from sweep_builder.data_containers.entity_node import EntityNode
+
+
+def test_add_child():
+    node = EntityNode('adset-id', Entity.AdSet)
+
+    node.add_node(EntityNode('ad-id1', Entity.Ad))
+
+    assert ['ad-id1'] == [n.entity_id for n in node.children]
+
+
+def test_add_child_with_path():
+    node = EntityNode('ad-account-id', Entity.AdAccount)
+
+    node.add_node(EntityNode('ad-id', Entity.Ad), ('campaign-id', 'adset-id'))
+
+    campaign_node = list(node.children)[0]
+    adset_node = list(campaign_node.children)[0]
+    ad_node = list(adset_node.children)[0]
+
+    assert 'campaign-id' == campaign_node.entity_id
+    assert 'adset-id' == adset_node.entity_id
+    assert 'ad-id' == ad_node.entity_id
