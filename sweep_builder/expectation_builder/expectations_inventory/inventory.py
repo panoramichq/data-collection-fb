4cb36823065d832ed81fba4872aaaef9b53f6d79
Mark Nemec <mrknmc@me.com>
2019-04-24 16:00:13 +0200

    Enable task breakdown for FabFitFun

--- sweep_builder/expectation_builder/expectations_inventory/inventory.py
+++ sweep_builder/expectation_builder/expectations_inventory/inventory.py
@@ -10,10 +10,6 @@ from config import jobs as jobs_config
 from common.enums.entity import Entity
 from common.enums.reporttype import ReportType
 from sweep_builder.expectation_builder.expectations_inventory.metrics.breakdowns import (
-    day_metrics_per_ad_per_entity,
-    hour_metrics_per_ad_per_entity,
-    day_platform_metrics_per_ad_per_entity,
-    day_age_gender_metrics_per_ad_per_entity,
     day_metrics_per_ads_under_ad_account,
 )
 from sweep_builder.expectation_builder.expectations_inventory.metrics.lifetime import (
@@ -43,9 +39,6 @@ from sweep_builder.expectation_builder.expectations_inventory.entities import (
     page_post_promotable_entities_per_page,
 )
 
-# map of source / trigger entity type to
-# a list of generator functions each of which, given RealityClaim instance
-# generate one or more ExpectationClaim objects
 entity_expectation_generator_map: Dict[str, List[ExpectationGeneratorType]] = {
     Entity.Scope: list(
         filter(
@@ -113,44 +106,5 @@ entity_expectation_generator_map: Dict[str, List[ExpectationGeneratorType]] = {
     ),
     Entity.PageVideo: list(
         filter(None, [None if jobs_config.INSIGHTS_LIFETIME_PV_DISABLED else lifetime_metrics_per_page_video])
-    ),
-}
-
-# mental note:
-# entities per AA data collection hook is as "normative" task on AA,
-# not an "effective" task under entity.
-# At some point it may be meaningful to have a normative "entity" job on each
-# entity level too / instead (where these jobs become "effective" alternatives there)
-
-
-# Special cases for ad account 23845179
-entity_expectations_for_23845179: Dict[str, List[ExpectationGeneratorType]] = {
-    Entity.AdAccount: list(
-        filter(
-            None,
-            [
-                None if jobs_config.ENTITY_AA_DISABLED else ad_account_entity,
-                None if jobs_config.ENTITY_C_DISABLED else campaign_entities_per_ad_account,
-                None if jobs_config.ENTITY_AS_DISABLED else adset_entities_per_ad_account,
-                None if jobs_config.ENTITY_A_DISABLED else ad_entities_per_ad_account,
-                None if jobs_config.ENTITY_AC_DISABLED else ad_creative_entities_per_ad_account,
-                None if jobs_config.ENTITY_AV_DISABLED else ad_video_entities_per_ad_account,
-                # None if jobs_config.ENTITY_CA_DISABLED else custom_audience_entities_per_ad_account,
-            ],
-        )
-    ),
-    Entity.Campaign: list(
-        filter(
-            None,
-            [
-                None if jobs_config.INSIGHTS_LIFETIME_C_DISABLED else lifetime_metrics_per_campaigns_under_ad_account,
-                None if jobs_config.INSIGHTS_LIFETIME_AS_DISABLED else lifetime_metrics_per_adsets_under_ad_account,
-                None if jobs_config.INSIGHTS_LIFETIME_A_DISABLED else lifetime_metrics_per_ads_under_ad_account,
-                None if jobs_config.INSIGHTS_DAY_A_DISABLED else day_metrics_per_ad_per_entity,
-                None if jobs_config.INSIGHTS_HOUR_A_DISABLED else hour_metrics_per_ad_per_entity,
-                None if jobs_config.INSIGHTS_AGE_GENDER_A_DISABLED else day_age_gender_metrics_per_ad_per_entity,
-                None if jobs_config.INSIGHTS_PLATFORM_A_DISABLED else day_platform_metrics_per_ad_per_entity,
-            ],
-        )
     ),
 }
