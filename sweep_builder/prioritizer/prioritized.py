a5cb22a5413b0519f72fdfdadfdbde4b64159b5b
Mark Nemec <mrknmc@me.com>
2019-05-09 15:11:24 +0200

    Remove caching from prioritized. No duplicate jobs

--- sweep_builder/prioritizer/prioritized.py
+++ sweep_builder/prioritizer/prioritized.py
@@ -1,8 +1,7 @@
 import time
 
-from typing import Generator, Iterable, Dict
+from typing import Generator, Iterable
 
-from common.job_signature import JobSignature
 from common.measurement import Measure
 from sweep_builder.data_containers.prioritization_claim import PrioritizationClaim
 from sweep_builder.data_containers.scorable_claim import ScorableClaim
@@ -15,8 +14,6 @@ def iter_prioritized(claims: Iterable[ScorableClaim]) -> Generator[Prioritizatio
 
     _before_next_expectation = time.time()
 
-    assigned_scores: Dict[JobSignature, int] = {}
-
     for claim in claims:
         _measurement_tags = {'entity_type': claim.entity_type, 'ad_account_id': claim.ad_account_id}
 
@@ -25,20 +22,6 @@ def iter_prioritized(claims: Iterable[ScorableClaim]) -> Generator[Prioritizatio
         )
 
         job_signature = claim.job_signature
-
-        # Cache already seen job_ids
-        cached_score = assigned_scores.get(job_signature)
-        if cached_score is not None:
-            yield PrioritizationClaim(
-                claim.entity_id,
-                claim.entity_type,
-                claim.report_type,
-                job_signature,
-                cached_score,
-                ad_account_id=claim.ad_account_id,
-                timezone=claim.timezone,
-            )
-            continue
 
         score = assign_score(claim)
 
@@ -53,5 +36,4 @@ def iter_prioritized(claims: Iterable[ScorableClaim]) -> Generator[Prioritizatio
                 timezone=claim.timezone,
             )
 
-        assigned_scores[job_signature] = score
         _before_next_expectation = time.time()
